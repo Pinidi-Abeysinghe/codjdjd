743a89a475588e5e9fd1d4e1f92d29c82a2e1da6
Pinidi-Abeysinghe <1997pinidi@gmail.com>
2022-04-05 23:50:07 +0530

    Initial Commit

---
+++ codjdjd/codjdjdApp.swift
@@ -0,0 +1,17 @@
+//
+//  codjdjdApp.swift
+//  codjdjd
+//
+//  Created by Pinidi Abeysinghe on 4/5/22.
+//
+
+import SwiftUI
+
+@main
+struct codjdjdApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
